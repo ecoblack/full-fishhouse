fac93fdd6d1dab796b31e8b9de0afd61d1ef0fc5
based_igor <igor.popelniuk@gmail.com>
2017-12-23 01:26:53 +0200

    added routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,10 @@
 class StaticPagesController < ApplicationController
-
- def home
-  	#@categories = Category.all
-  end
-
-  def about
+  def home
   end
 
   def contact
+  end
+
+  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+ get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
   resources :categories
   resources :products
 
 root to: 'products#index'
 #DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	
 end
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
+ 
