aafd770b5d459c46c614c0cdfd575b210ee36516
based_igor <igor.popelniuk@gmail.com>
2017-12-22 21:23:36 +0200

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :products
   resources :static_pages
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: 'products#index'
+#DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
