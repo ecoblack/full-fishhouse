aca2013bae03e313020dbc5279ce74f41d624a5e
based_igor <igor.popelniuk@gmail.com>
2017-12-23 00:53:05 +0200

    added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :categories
   resources :products
-  resources :static_pages
+
 root to: 'products#index'
 #DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
