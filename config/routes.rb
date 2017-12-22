467c25ccada92baaaff81c9ebc105e0eebe3679a
based_igor <igor.popelniuk@gmail.com>
2017-12-23 01:45:38 +0200

    home page test

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
- get  '/about',   to: 'static_pages#about'
+  root 'static_pages#home'
+
+  get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+
   resources :categories
   resources :products
 
-root to: 'products#index'
 #DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	
 end
