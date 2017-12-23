48c414905ddb8638abbb828c83809831f30c20e1
based_igor <igor.popelniuk@gmail.com>
2017-12-23 12:25:24 +0200

    remove active admin comments 3

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,15 +1,21 @@
 ActiveAdmin.register Category do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+		permit_params :name, :body,:parent_id, 
 
+		show do |t|
+			attributes_table do
+				row :name
+				row :parent_id
+				row :body
+				end
+			end
+		end
+
+		form :html => { :enctype => "multipart/form-data" } do |f|
+			f.inputs do
+				f.input :name
+				f.input :parent_id
+				f.input :body
+			end
+			f.actions
+		end
 end
