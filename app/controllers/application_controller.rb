864cc27537b0e09a02b84ec049261e209f0e1691
based_igor <igor.popelniuk@gmail.com>
2017-12-24 00:16:12 +0200

    Added more styling 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+	def index
+   		 @category = Category.find(params[:id])
+  	
+
+		  @products = Product.search(params[:search]).order(sort_column + " " + sort_direction).paginate(:per_page => 5, :page => params[:page])
+		end
+
+
+
 end
