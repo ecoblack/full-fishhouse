e6df21a7aee4cd5ec9df8d978365eaa5eed517fd
based_igor <igor.popelniuk@gmail.com>
2018-01-16 10:37:39 +0200

    Added search

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,6 +2,27 @@ document.addEventListener("turbolinks:load", function() {
   $input = $("[data-behavior='autocomplete']")
 
   var options = {
+    getValue: "name",
+    url: function(phrase) {
+      return "/search.json?q=" + phrase;
+    },
+    categories: [
+      {
+        listLocation: "products",
+        header: "<strong>Продукция:</strong>",
+      },
+      {
+        listLocation: "categories",
+        header: "<strong>Категории:</strong>",
+      }
+    ],
+    list: {
+      onChooseEvent: function() {
+        var url = $input.getSelectedItemData().url
+        $input.val("")
+        Turbolinks.visit(url)
+      }
+    }
   }
 
   $input.easyAutocomplete(options)
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,15 @@
 class MainController < ApplicationController
-	before_action :force_json, only: :search
+	before_action :force_json, only: :autocomplete
 
-	def search
+
+def autocomplete
 		@products = Product.ransack(name_cont: params[:q]).result(distinct: true)
 		@categories = Category.ransack(name_cont: params[:q]).result(distinct: true)
+	end
+
+	def search
+		@products = Product.ransack(name_cont: params[:q]).result(distinct: true).limit(5)
+		@categories = Category.ransack(name_cont: params[:q]).result(distinct: true).limit(5)
 	end
 
 
